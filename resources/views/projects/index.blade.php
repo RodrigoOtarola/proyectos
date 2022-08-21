8f2aa27b2da4636b1ef17b3511a0771a16cb34b8
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-08-20 21:07:54 -0400

    vista projects.index, falta arreglar lista

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,17 +3,24 @@
 @section('title', 'Project')
 
 @section('container')
-    <h2>Hola desde portafolio</h2>
+    <div class="container">
+        <h2 class="display-4">Portafolio</h2>
 
-    @auth()
-        <button><a href="{{route('projects.create')}}">Crear proyecto.</a></button>
-    @endauth
+        @auth()
+            <button><a href="{{route('projects.create')}}">Crear proyecto.</a></button>
+        @endauth
 
-    <ul>
-        @forelse($projects as $project)
-            <li><a href="{{route('projects.show', $project->id)}}">{{ $project->title}}</a></li>
-        @empty
-            <li>No hay proyectos para mostrar.</li>
-        @endforelse
-    </ul>
+        <ul class="list-group">
+            @forelse($projects as $project)
+                <li class="list-group-item border-0 mb-3 shadow-sm">
+                    <a class="d-flex" href="{{route('projects.show', $project->id)}}">
+                        {{ $project->title}}
+                        {{$project->created_at->format('d/m/Y')}}
+                    </a>
+                </li>
+            @empty
+                <li>No hay proyectos para mostrar.</li>
+            @endforelse
+        </ul>
+    </div>
 @endsection
